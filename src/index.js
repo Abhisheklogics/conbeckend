5f7f148bbd21a41d91d4d394c7aeb0ea5600eacc
Abhishek Kumar <abhi.kumar252004@gmail.com>
2025-01-01 13:16:54 +0530

    all

--- src/index.js
+++ src/index.js
@@ -20,36 +20,56 @@ const io = new Server(server, {
   cors: true,
 });
 
+let users = []; // Keep track of connected users
+
 io.on("connection", (socket) => {
   console.log("A user connected:", socket.id);
 
+  // Add user to the list
+  users.push(socket.id);
+
+  // Notify all users about the new user
   socket.broadcast.emit("new-user", socket.id);
 
+  // When a user starts screen sharing
+  socket.on("start-screen-sharing", () => {
+    console.log("Screen sharing started by", socket.id);
+
+    // Broadcast screen-sharing start to all users
+    io.emit("screen-sharing-started", { id: socket.id });
+  });
+
+  // When a user stops screen sharing
+  socket.on("stop-screen-sharing", () => {
+    console.log("Screen sharing stopped by", socket.id);
+
+    // Broadcast screen-sharing stop to all users
+    io.emit("stop-screen-sharing");
+  });
+
+  // Handle offer (screen sharing offer)
   socket.on("offer", (data) => {
     io.to(data.id).emit("offer", { id: socket.id, description: data.description });
   });
 
+  // Handle answer (response to the offer)
   socket.on("answer", (data) => {
     io.to(data.id).emit("answer", { id: socket.id, description: data.description });
   });
 
+  // Handle ICE candidates
   socket.on("ice-candidate", (data) => {
     io.to(data.id).emit("ice-candidate", { id: socket.id, candidate: data.candidate });
   });
 
-  socket.on("start-screen-sharing", () => {
-    console.log("Screen sharing started by", socket.id);
-    // Notify other users that screen sharing has started
-    socket.broadcast.emit("screen-sharing-started", { id: socket.id });
-  });
-
-  socket.on("stop-screen-sharing", () => {
-    console.log("Screen sharing stopped by", socket.id);
-    socket.broadcast.emit("stop-screen-sharing");
-  });
-
+  // Handle user disconnection
   socket.on("disconnect", () => {
     console.log("A user disconnected:", socket.id);
+
+    // Remove user from the list
+    users = users.filter((user) => user !== socket.id);
+
+    // Notify other users that this user has disconnected
     socket.broadcast.emit("user-disconnected", socket.id);
   });
 });
