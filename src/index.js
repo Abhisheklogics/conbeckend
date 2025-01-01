058f08ef2820b045df563bae7628452fa3a6def4
Abhishek Kumar <abhi.kumar252004@gmail.com>
2025-01-01 12:53:08 +0530

    all

---
+++ src/index.js
@@ -0,0 +1,60 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    methods: ["GET", "POST"],
+  })
+);
+
+const io = new Server(server, {
+  cors: true,
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
+  socket.broadcast.emit("new-user", socket.id);
+
+  socket.on("offer", (data) => {
+    io.to(data.id).emit("offer", { id: socket.id, description: data.description });
+  });
+
+  socket.on("answer", (data) => {
+    io.to(data.id).emit("answer", { id: socket.id, description: data.description });
+  });
+
+  socket.on("ice-candidate", (data) => {
+    io.to(data.id).emit("ice-candidate", { id: socket.id, candidate: data.candidate });
+  });
+
+  socket.on("start-screen-sharing", () => {
+    console.log("Screen sharing started by", socket.id);
+    // Notify other users that screen sharing has started
+    socket.broadcast.emit("screen-sharing-started", { id: socket.id });
+  });
+
+  socket.on("stop-screen-sharing", () => {
+    console.log("Screen sharing stopped by", socket.id);
+    socket.broadcast.emit("stop-screen-sharing");
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected:", socket.id);
+    socket.broadcast.emit("user-disconnected", socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
