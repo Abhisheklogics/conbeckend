f406bdbeb7ea7ce30ae47b72df1e39f3b601a29a
Abhishek Kumar <abhi.kumar252004@gmail.com>
2025-01-06 12:16:14 +0530

    al

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: 'conexusapp.netlify.app',
     methods: ["GET", "POST"],
   })
 );
