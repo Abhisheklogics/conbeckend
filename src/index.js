0f76bde1fad365d695b6999c9d242bfefbb76732
Abhishek Kumar <166478718+Abhisheklogics@users.noreply.github.com>
2025-01-26 13:03:18 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -18,63 +18,52 @@ app.use(
 const server = createServer(app);
 const io = new Server(server, { cors: true });
 
-const rooms = {}; // Store rooms with the code
+const rooms = {}; // Structure: { roomCode: { master: socketId, slaves: [] } }
 
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
-  // Master joins the room
-  socket.on("master", (data) => {
-    const { email, code } = data;
-
+  // Master joins
+  socket.on("master", ({ email, code }) => {
     if (email === "amarjeetsinghchauhan96@gmail.com") {
       rooms[code] = { master: socket.id, slaves: [] };
-      socket.emit("roomCode", { code });
+      socket.join(code); // Join master to the room
+      socket.emit("roomCreated", { code });
     }
   });
 
-  // Slave joins the room
-  socket.on("slave", (data) => {
-    const { code } = data;
-
+  // Slave joins
+  socket.on("slave", ({ code }) => {
     if (rooms[code]) {
       rooms[code].slaves.push(socket.id);
-      socket.emit("validCode", { code });
-
-      io.to(rooms[code].master).emit("updateSlaveCount", rooms[code].slaves.length);
+      socket.join(code); // Join slave to the room
+      io.to(code).emit("updateSlaveCount", rooms[code].slaves.length); // Notify master of updated count
     } else {
       socket.emit("invalidCode", { message: "Invalid room code." });
     }
   });
 
-  // Relay screen stream data to all slaves in the room
-  socket.on("screenStream", (data) => {
-    const { roomCode, streamData } = data;
-
-    const slaves = rooms[roomCode]?.slaves;
-    if (slaves && slaves.length > 0) {
-      slaves.forEach((slaveId) => {
-        io.to(slaveId).emit("screenStream", { streamData });
-      });
-    }
+  // Master shares screen stream
+  socket.on("screenStream", ({ roomCode, streamData }) => {
+    io.to(roomCode).emit("screenStream", { streamData }); // Broadcast to all slaves
   });
 
-  // Handle disconnection
+  // Handle disconnect
   socket.on("disconnect", () => {
     Object.keys(rooms).forEach((code) => {
+      // Remove disconnected user from the room
       rooms[code].slaves = rooms[code].slaves.filter((id) => id !== socket.id);
+
+      // If master disconnects, delete the room
       if (rooms[code].master === socket.id) {
         delete rooms[code];
-      }
-
-      if (rooms[code].master) {
-        io.to(rooms[code].master).emit("updateSlaveCount", rooms[code].slaves.length);
+      } else {
+        io.to(code).emit("updateSlaveCount", rooms[code].slaves.length);
       }
     });
     console.log("User disconnected:", socket.id);
   });
 });
-
 
 server.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
