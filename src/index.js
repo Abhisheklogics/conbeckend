d723e80955aec89b29c7cd3067e4c992cf20e32b
Abhishek Kumar <166478718+Abhisheklogics@users.noreply.github.com>
2025-02-06 13:11:54 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['https://conexus-meet.vercel.app/'],
     methods: ['GET', 'POST'],
     credentials: true,
 }));
